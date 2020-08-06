3768249dfb56691da047849ff54ad33af3c77444
fooooooooooooooo <sessingd@gmail.com>
2020-08-06 15:25:04 -0700

    s

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const app = express();
 const request = require('request');
 const port = process.env.PORT || 3000;
@@ -8,7 +8,12 @@ if (process.platform === 'win32') {
     const dotenv = require('dotenv');
     dotenv.config();
 }
-app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/avatar/', async function (req, response) {
     let id = req.query.id;
